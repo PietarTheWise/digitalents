3ef5b445b633000044fdd100b3d919816b8b1dac
Pietari <pietari.noutere@gmail.com>
2020-02-17 14:58:29 +0200

    some addition

---
+++ javascript_net_ninja/excercise_17022020/app.js
@@ -0,0 +1,30 @@
+
+
+// const area = 7.7;
+
+// console.log(Math.floor(area));
+// console.log(Math.random(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+
+
+let template = [1, 3, 2, 5, 6]
+const random = Math.random() * 10;
+
+
+    
+const test = template.map(function(){ 
+    return Math.floor(random)
+});
+
+console.log(test)
+
+// for (let i = 0; i < template.length; i++){
+//     template[i].map(e => e Math.floor(random))
+// }
+
+
+
+
+
+// console.log(template);
